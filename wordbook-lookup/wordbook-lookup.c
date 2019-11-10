2c917368bc5efd2fac913666d3c843bc70f78cc0
Christian M. Jensen <cmj@cjpg.dk>
2019-11-10 17:51:59 +0100

    Enable mem leak detect

--- wordbook-lookup/wordbook-lookup.c
+++ wordbook-lookup/wordbook-lookup.c
@@ -1,9 +1,19 @@
+
 #include <stdio.h>
 #include"wordbooklib.h"
 #include <json-c/json.h>
+#if _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 
 int main(int argc, char *argv[])
 {
+#if _DEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
     struct curl_download_result json_result;
     json_result = wordbook_get_dictionaries_json();
 
@@ -11,7 +21,9 @@ int main(int argc, char *argv[])
     {
         struct json_object *json_obj;
         json_obj = json_tokener_parse(json_result.ptr);
+
         printf("Result after parse:\n%s\n\n", json_object_to_json_string(json_obj));
+        json_object_put(json_obj);
         free(json_result.ptr);
     }
 
