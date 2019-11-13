b70214cbd439f7a8bec7643cc1d72518f3570e75
Christian M. Jensen <cmj@cjpg.dk>
2019-11-13 18:10:08 +0100

    format

--- wordbook-lookup/wordbook-lookup.h
+++ wordbook-lookup/wordbook-lookup.h
@@ -4,6 +4,6 @@
 void print_help_text(const char *app);
 void print_all_dictionaries(const char *search);
 // convert all chars to lower and reutrn strstr (Return cannot be used! Only checked!)
-char* strstr_i(const char *s1, const char *s2);
+char *strstr_i(const char *s1, const char *s2);
 
 #endif // WORDBOOK_LOOKUP_H
